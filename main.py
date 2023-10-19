165518126e890a1d22fb08824dac074a546768be
FEZEKIL <gxalaba.fezekile@gmail.com>
2023-10-19 18:18:42 +0200

    Send email with news titles and description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 
 api_key = "b45bf6128ec44bbaa2f9b24706eef0c4"
@@ -8,10 +9,16 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=2023-09-19&sortBy=publishe
 # Make a request
 request = requests.get(url)
 
-#Get a dictionary with dara
+#Get a dictionary with data
 content = request.json()
 
-# Acceses the article title and description
+# Acces the article title and description
+body = ""
 for article in (content["articles"]):
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+
+
+body = body.encode("utf-8")
+send_email(message=body)
+
